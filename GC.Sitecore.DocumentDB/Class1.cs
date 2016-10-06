922e40f9fd78a42815ed889a0b135cb08066cfb8
George Chang <george.chang@perficient.com>
2016-10-06 14:57:27 -0400

    Add project files.

---
+++ GC.Sitecore.DocumentDB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC.Sitecore.DocumentDB
+{
+    public class Class1
+    {
+    }
+}
