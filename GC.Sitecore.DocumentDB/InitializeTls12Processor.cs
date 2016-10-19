117b968bbc000bf18e986171838503dae8cf9aaf
George Chang <changg@gmail.com>
2016-10-19 13:41:59 -0400

    Initial commit

---
+++ GC.Sitecore.DocumentDB/InitializeTls12Processor.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Authentication;
+using MongoDB.Driver;
+using Sitecore.Analytics.Pipelines.UpdateMongoDriverSettings;
+
+namespace GC.Sitecore.DocumentDB
+{
+    public class InitializeTls12Processor : UpdateMongoDriverSettingsProcessor
+    {
+        public override void UpdateSettings(UpdateMongoDriverSettingsArgs args)
+        {
+            //args.MongoSettings.SslSettings = new SslSettings {EnabledSslProtocols = SslProtocols.Tls12};
+        }
+    }
+}
