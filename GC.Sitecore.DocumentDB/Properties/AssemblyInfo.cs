aaccf3c084f89d083aa4795d4fdb0b928f93fe93
George Chang <changg@gmail.com>
2016-12-10 18:56:14 -0500

    Update project to be web project

--- GC.Sitecore.DocumentDB/Properties/AssemblyInfo.cs
+++ GC.Sitecore.DocumentDB/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b4afadfa-6b11-4715-8e71-d06170592b48")]
+[assembly: Guid("b9707b19-b080-41a3-bd70-9ccc17006b0d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
